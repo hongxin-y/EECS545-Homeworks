566a6469e2efb36d95ed4ed3d0f48dcf907c1fc0
Erebos <erebos@rams.colostate.edu>
2019-10-07 08:30:18 +0800

    add hw3 p2 and fix some bugs in p1

--- HW3/cross_vali_ridge.py
+++ HW3/cross_vali_ridge.py
@@ -9,35 +9,31 @@ def load_data(filename):
     X = data.drop('CMEDV', axis = 1)
     X = np.array(X).T
     Y = np.array(Y)
-    print(Y.shape)
     X_train = X[:,:-46]
     Y_train = Y[:-46]
     X_test = X[:,-46:]
     Y_test = Y[-46:]
     return X_train, Y_train, X_test, Y_test
 
-def sphere(X, Y):
-    n = Y.shape[0]
+def sphere(X):
+    n = X.shape[1]
     X_mu = np.mean(X, 1)
-    Y_mu = np.mean(Y)
     X_sigma2 = (np.mean(X*X, 1) - (X_mu*X_mu))*(n/(n-1))
-    Y_sigma2 = np.sum((Y - Y_mu)*(Y- Y_mu))/(n-1)
-    X = (X - X_mu[:, None]) / X_sigma2[:,None]
-    Y = (Y - Y_mu)/Y_sigma2
-    return X, Y, X_mu, Y_mu, X_sigma2, Y_sigma2
+    X = (X - X_mu[:, None]) / np.sqrt(X_sigma2[:,None])
+    return X, X_mu, X_sigma2
 
 def train(X_train, Y_train, lam):
     X_train = np.r_[np.array([np.ones(X_train.shape[1])]), X_train]
+    Y_train = np.array([Y_train])
     p, n = X_train.shape[0] - 1, X_train.shape[1]
     I_hat = np.eye(p+1)
     I_hat[0][0] = 0
     A = np.dot(X_train, X_train.T) + lam*I_hat
     L = np.dot(np.linalg.inv(A), X_train)
-    w = np.dot(L, Y_train)
+    w = np.dot(L, Y_train.T)
     df = np.trace(np.dot(X_train.T, L))
 
-    Y_hat = np.dot(X_train.T, w)
-    #print(Y_hat)
+    Y_hat = np.dot(w.T, X_train)
     error = mse(Y_hat, Y_train)
     return w, df, error
 
@@ -47,30 +43,32 @@ def mse(Y_hat, Y_train):
 
 def test(X_test, Y_test, w):
     X_test = np.r_[np.array([np.ones(X_test.shape[1])]), X_test]
-    Y_hat = np.dot(X_test.T, w)
+    Y_hat = np.dot(w.T, X_test)
     error = mse(Y_hat, Y_test)
     return error
 
 def k_cross_validation(K, lam, X_train_org, Y_train_org):
     CV_error = 0
     dk = Y_train_org.shape[0]//K
-    print(dk)
     for k in range(K):
         X_train = np.c_[X_train_org[:,:k],X_train_org[:,k+dk:]]
         X_valid = X_train_org[:,k:k+dk]
         Y_train = np.r_[Y_train_org[:k],Y_train_org[k+dk:]]
         Y_valid = Y_train_org[k:k+dk]
-        X_train, Y_train = sphere(X_train, Y_train)[:2]
-        X_valid, Y_valid = sphere(X_valid, Y_valid)[:2]
+        X_train = sphere(X_train)[0]
+        X_valid = sphere(X_valid)[0]
         w = train(X_train, Y_train, lam)[0]
         e = test(X_valid, Y_valid, w)
         CV_error += e
-    print("cv error with lam =", lam, "is", CV_error/K)
-    return CV_error/K
+    CV_error /= K
+    print("cv error with lambda = {:d} is {:.5f}".format(lam, CV_error,))
+    return CV_error
 
 X_train_org, Y_train_org, X_test, Y_test = load_data("boston-corrected")
-X_train_valid, Y_train_valid, X_train_mu, Y__train_mu, X_train_sigma2, Y_train_sigma2 = sphere(X_train_org, Y_train_org)
-#print(X,Y)
+X_train_valid, X_train_mu, X_train_sigma2 = sphere(X_train_org)
+Y_train_valid = Y_train_org
+print("X_mu vector is ", X_train_mu)
+print("X_sigma2 vector is ", X_train_sigma2)
 
 dfs = []
 errors = []
@@ -91,17 +89,17 @@ plt.plot(range(21), errors)
 plt.show()
 
 w = train(X_train_valid, Y_train_valid, 0)[0]
-X_test, Y_test = sphere(X_test, Y_test)[0:2]
+X_test = sphere(X_test)[0]
 e = test(X_test, Y_test, w)
-print("mse in test set:", e)
+print("mse in test set: {:.5f}".format(e))
 
 lam_star = 0
 df_star = 0
 cve_min = float('inf')
-for lam in range(21):
+for lam in range(201):
     cve = k_cross_validation(10, lam, X_train_org, Y_train_org)
     if cve < cve_min:
         cve_min = cve
         lam_star = lam
         df_star = train(X_train_valid, Y_train_valid, lam)[1]
-print(lam_star, df_star, cve_min)
+print("lambda = {:d}, df = {:.2f}, CV error = {:.5f}".format(lam_star, df_star, cve_min))
---
+++ HW3/gradient_descent.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import scipy.io as scio
+import matplotlib.pyplot as plt
+
+class linear_regression():
+    def __init__(self, X, Y, learning_rate = 0.05):
+        self.lr = learning_rate
+        self.p, self.n = X.shape
+        self.X, self.Y = sphere(X, Y)
+        self.X = np.r_[np.array([np.ones(self.n)]), self.X]
+        self.Y = np.array([self.Y])
+        self.w = np.array([np.zeros(self.p+1)]).T
+
+    def gradient_descent(self):
+        grad = (2/self.n) * np.dot(self.X, (np.dot(self.w.T, self.X) - self.Y).T)
+        self.w = self.w - self.lr*grad
+
+    def stochastic_GD(self):
+        X = self.X
+        Y = self.Y
+        for i in range(self.n):
+            grad = np.array([X[:,i]*(np.dot(self.w.T, X[:,i]) - Y[:,i])]).T
+            self.w = self.w - self.lr*grad
+
+    def matrix_method(self):
+        X = self.X
+        Y = self.Y
+        self.w = np.dot(np.dot(np.linalg.pinv(np.dot(X, X.T)), X), Y.T)
+
+    def fit(self, X_test):
+        X_test = np.r_[np.array([np.ones(X_test.shape[1])]), X_test]
+        return np.dot(self.w.T, X_test)
+
+def load_data(filename):
+    data = pd.read_csv(filename)
+    Y = data['CMEDV']
+    X = data.drop('CMEDV', axis = 1)
+    X = np.array(X).T
+    Y = np.array(Y)
+    X_train = X[:,:-46]
+    Y_train = Y[:-46]
+    X_test = X[:,-46:]
+    Y_test = Y[-46:]
+    return X_train, Y_train, X_test, Y_test
+
+def sphere(X, Y):
+    n = Y.shape[0]
+    X_mu = np.mean(X, 1)
+    X_sigma2 = (np.mean(X*X, 1) - (X_mu*X_mu))*(n/(n-1))
+    X = (X - X_mu[:, None]) / np.sqrt(X_sigma2[:,None])
+    return X, Y
+
+def mse(Y_hat, Y):
+    resid = Y - Y_hat
+    return np.mean(resid*resid)
+
+def train(model, method = "BGD", K = 500):
+    if method == "BGD":
+        for _ in range(K):
+            model.gradient_descent()
+    elif method == "SGD":
+        for _ in range(K):
+            model.stochastic_GD()
+    elif method == "MAT":
+        model.matrix_method()
+    return model
+
+def model_display(filename, method = "BGD", lr = 0.05):
+    X_train, Y_train, X_test, Y_test = load_data(filename)
+    model = linear_regression(X_train, Y_train, lr)
+    if method == "BGD":
+        model = train(model, method, 500)
+    elif method == "SGD":
+        model = train(model, method, 500)
+    elif method == "MAT":
+        model = train(model, method)
+    X_test, Y_test = sphere(X_test, Y_test)
+    Y_hat = model.fit(X_test)
+    error = mse(Y_hat, Y_test)
+    #print(model.w.T)
+    print("mse on test set based on {} is {:5f}".format(method, error))
+
+model_display("boston-corrected", method = "BGD", lr = 0.05)
+model_display("boston-corrected", method = "SGD", lr = 0.0005)
+model_display("boston-corrected", method = "MAT")
